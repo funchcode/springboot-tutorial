fc1bbee67b0109fb9cc97e9f08d6d2c7b8194011
정채환 <funchcode@gmail.com>
2019-03-20 13:53:06 +0900

    리스트 불러오기(DTO, SERVICE, REPOSITORY)

--- src/main/java/com/ririshop/program/service/PostsService.java
+++ src/main/java/com/ririshop/program/service/PostsService.java
@@ -1,11 +1,14 @@
 package com.ririshop.program.service;
 
 import com.ririshop.program.domain.posts.PostsRepository;
+import com.ririshop.program.dto.PostsMainResponseDto;
 import com.ririshop.program.dto.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -15,5 +18,10 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().map(PostsMainResponseDto::new).collect(Collectors.toList());
     }
 }
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,2 @@
+INSERT INTO POSTS (title, author, content) VALUES ('테스트1', 'test1@gmail.com', '테스트1의 본문');
+INSERT INTO POSTS (title, author, content) VALUES ('테스트2', 'test2@gmail.com', '테스트2의 본문');
