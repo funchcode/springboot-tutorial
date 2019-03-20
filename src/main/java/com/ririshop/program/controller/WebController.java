46e6a74152485f966dddc8792a8cddeace975ac9
정채환 <funchcode@gmail.com>
2019-03-20 10:06:31 +0900

    Handlebars 적용 및 테스트

---
+++ src/main/java/com/ririshop/program/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.ririshop.program.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
